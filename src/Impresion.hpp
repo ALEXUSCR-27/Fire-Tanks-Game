a646330096ba41fe396e1f72a7d3e43c35719074
ALEXUSCR-27 <asmalexander@hotmail.com>
2022-06-01 20:59:42 -0600

    Dia 2

--- src/Impresion.hpp
+++ src/Impresion.hpp
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include "Jugador.hpp"
 using namespace std;
 
 /*****Nombre*************************************** 
@@ -37,9 +38,22 @@ class Impresion {
 			aSBSize=GetLargestConsoleWindowSize(aHConsole);
         }
     void ImpresionPantalla1() {
+        system("cls");
         char MsgBienvenida[] = "[BIENVENIDOS A FIRE TANKS]";
-        ImprimirCentral(MsgBienvenida);
-        //ImprimirTanque();
+        CentrarCursor(MsgBienvenida);
+		Sleep(10);
+		for (int i=0;i<24;i++) cout<<"\n";
+		int msg=strlen(MsgBienvenida);
+		int pos=(int)(((aSBSize.X)-msg)/2);
+		for(int i=0;i<pos;i++) cout<<" ";
+		SetConsoleTextAttribute(aHConsole,2);
+		for (int i=0;i!=msg;i++) {
+			cout<<MsgBienvenida[i];
+			Sleep(50);
+		}
+        cout<<endl<<endl<<endl;
+        ImprimirTanque();
+        cout<<endl;
         ImprimirContinuar();
 
     }
@@ -65,43 +79,26 @@ class Impresion {
 		for(int i=0;i<pos;i++) cout<<" ";
 	}
 
-    void ImprimirCentral(char *s) {
-		system("cls");
-		Sleep(10);
-		for (int i=0;i<24;i++) cout<<"\n";
-		int msg=strlen(s);
-		int pos=(int)(((aSBSize.X)-msg)/2);
-		for(int i=0;i<pos;i++) cout<<" ";
-		SetConsoleTextAttribute(aHConsole,2);
-		for (int i=0;i!=msg;i++) {
-			cout<<s[i];
-			Sleep(50);
-		}
-        cout<<endl;
-	}
 
     void ImprimirTanque() {
-        int x = 255;
-        char parte1[] = "██████**********************";
-        char parte2[] = "          ";
-        char parte3[] = "  ■■ ■■■■■■■■ ■■■      ";
-        char parte4[] = "              ";
+        char parte1[] = "    ________       ";
+        char parte2[] = "        __/        \\_____________";
+        char parte3[] = "              |___         _____________|=====|>";
+        char parte4[] = "______|       |_________";
+        char parte5[] = "|________________________|";
+        char parte6[] = "\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/";
         CentrarCursor(parte1);
-        SetConsoleTextAttribute(aHConsole,240);
         cout<<parte1<<endl;
-        SetConsoleTextAttribute(aHConsole,2);
         CentrarCursor(parte2);
-        SetConsoleTextAttribute(aHConsole,240);
         cout<<parte2<<endl;
-        SetConsoleTextAttribute(aHConsole,2);
         CentrarCursor(parte3);
-        SetConsoleTextAttribute(aHConsole,240);
         cout<<parte3<<endl;
-        SetConsoleTextAttribute(aHConsole,2);
         CentrarCursor(parte4);
-        SetConsoleTextAttribute(aHConsole,240);
         cout<<parte4<<endl;
-        SetConsoleTextAttribute(aHConsole,2);
+        CentrarCursor(parte5);
+        cout<<parte5<<endl;
+        CentrarCursor(parte6);
+        cout<<parte6<<endl;
 
     }
 
@@ -113,14 +110,70 @@ class Impresion {
     }
 
     void ImpresionObtenerNombres() {
-        for (int i=0;i<24;i++) cout<<"\n";
+        for (int i=0;i<20;i++) cout<<"\n";
         char MsgTitulo1[] = "[INGRESE EL NOMBRE DE LOS JUGADORES]";
         CentrarCursor(MsgTitulo1);
         cout<<MsgTitulo1<<endl;
-        for (int i=0;i<10;i++) cout<<"\n";
+        cout<<"\n\n\n";
         char MsgJugador1[] = "[JUGADOR 1] = > ";
         CursorIzquierda(MsgJugador1);
         cout<<MsgJugador1;
+    }
+    void ImpresionObtenerNombres2() {
+        cout<<"\n\n\n";
+        char MsgJugador2[] = "[JUGADOR 2] = > ";
+        CursorIzquierda(MsgJugador2);
+        cout<<MsgJugador2;
+    }
+    void ImpresionDificultad() {
+        cout<<endl;
+        char MsgDificultad[] = "[MODO DE JUEGO]";
+        CentrarCursor(MsgDificultad);
+        cout<<MsgDificultad<<"\n\n";
+        char MsgDificultad2[] ="[FACIL]==>1         [DIFICIL]==>2";
+        CentrarCursor(MsgDificultad2);
+        cout<<MsgDificultad2<<"\n\n";
+        char MsgDificultad3[] = "[SELECCIONE EL MODO DE JUEGO]==>";
+        CentrarCursor(MsgDificultad3);
+        cout<<MsgDificultad3;
+    }
+
+    void ImpresionError01() {
+        char MsgError01[] = "[POR FAVOR DIGITE CORRECTAMENTE UNO DE LOS MODOS DISPONIBLES]==>";
+        CentrarCursor(MsgError01);
+        cout<<MsgError01;
+    }
+
+    void ImpresionTanques(Jugador *J1) {
+        char MsgTanques[] = "[SELECCIONE EL TANQUE QUE DESEA UTILIZAR]";
+        for (int i=0;i<20;i++) cout<<"\n";
+        CentrarCursor(MsgTanques);
+        cout<<MsgTanques<<endl;
+        char Tanque1[] = " [1].[T-28 SUPER TANK]===>[100 VIDA - 30 DAÑO]";
+        char Tanque2[] = "[2].[T-14 ARMATA]===>[100 VIDA - 60 DAÑO]";
+        char Tanque3[] = "[3].[LEOPARDO II]===>[150 VIDA - 15 DAÑO]";
+        char Tanque4[] = "[4].[BLACK NIGHT]===>[100 VIDA - 25 DAÑO]";
+        char InputTanque[] = "==>";
+        string nombreJ1 = J1->GetNombre();
+        cout<<"\n\n";
+        CursorIzquierda(Tanque1);
+        printf(" [1].[T-28 SUPER TANK]===>[100 VIDA - 30 DA%cO]\n",165);
+        CursorIzquierda(Tanque2);
+        printf("[2].[T-14 ARMATA]===>[100 VIDA - 60 DA%cO]\n",165);
+        CursorIzquierda(Tanque3);
+        printf("[3].[LEOPARDO II]===>[150 VIDA - 15 DA%cO]\n",165);
+        CursorIzquierda(Tanque4);
+        printf("[4].[BLACK NIGHT]===>[100 VIDA - 25 DA%cO]\n",165);
+        CursorIzquierda(Tanque4);
+        cout<<"["<<nombreJ1<<"] "<<InputTanque;
+    }
+
+    void ImpresionTanques02(Jugador *J2) {
+        char Tanque4[] = "[4].[BLACK NIGHT]===>[100 VIDA - 25 DAÑO]";
+        string nombreJ2 = J2->GetNombre();
+        char InputTanque[] = "==>";
+        CursorIzquierda(Tanque4);
+        cout<<"["<<nombreJ2<<"] "<<InputTanque;
     }
 
 };
--- src/Main.cpp
+++ src/Main.cpp
@@ -8,14 +8,20 @@ using namespace std;
 #define DEFAULT_BUFLEN 512
 
 Impresion *gImpresion = new Impresion();
-Jugador *gJugador1 = new Jugador();
-Jugador *gJugador2 = new Jugador();
+Jugador *gJugador1 = nullptr;
+Jugador *gJugador2 = nullptr;
+int gDificultad = 0;
 //Variables globales para impresion
 HANDLE gHOut=GetStdHandle(STD_OUTPUT_HANDLE);
 COORD gSBSize=GetLargestConsoleWindowSize(gHOut);
 
 void Inicio();
 void Menu();
+void Menu02();
+void Menu03();
+void Menu03_2();
+int VerificarNumero();
+
 
 int main() {
     system("cls");
@@ -31,10 +37,70 @@ void Inicio() {
 }
 
 void Menu() {
+    gJugador1 = new Jugador();
+    gJugador2 = new Jugador();
     gImpresion->ImpresionObtenerNombres();
     string nombre1;
     cin>>nombre1;
     gJugador1->SetNombre(nombre1);
-    cout<<gJugador1->GetNombre();
+    gImpresion->ImpresionObtenerNombres2();
+    string nombre2;
+    cin>>nombre2;
+    gJugador2->SetNombre(nombre2);
+    gImpresion->ImpresionDificultad();
+    Menu02();
+    
+}
+
+void Menu02() {
+    gDificultad = VerificarNumero();
+    switch(gDificultad) {
+        case 1:
+            gDificultad = 1;
+            break;
+        case 2:
+            gDificultad = 2;
+            break;
+        default:
+            gImpresion->ImpresionError01();
+            Menu02();
+    }
+    Menu03();
+}
+
+void Menu03() {
+    system("cls");
+    gImpresion->ImpresionTanques(gJugador1);
+    Menu03_2();
+    int TanqueJ1 = VerificarNumero();
+}
+
+void Menu03_2() {
+    int TanqueJ1 = VerificarNumero();
+    //Verificar
+    gImpresion->ImpresionTanques02(gJugador2);
+    int TanqueJ2 = VerificarNumero();
     system("pause");
+
+}
+
+
+
+/*****Nombre***************************************
+* VerificarNumero
+*****Descripción**********************************
+* Metodo para verificar si el usuario ingresa un numero
+*****Retorno**************************************
+* Un numero entero
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+int VerificarNumero() {
+	int numero;
+	while (!(cin>>numero)) {
+		cin.clear();
+		cin.ignore(10000,'\n');
+        gImpresion->ImpresionError01();
+	}
+	return numero;
 }
